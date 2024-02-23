90784de0285917a7aea7b679cd8f45e7b2c02b23
AnasUsman70 <anasusman598@gmail.com>
2024-02-23 16:37:52 +0500

    js class 1

---
+++ app.js
@@ -0,0 +1,112 @@
+// assignment 1
+// chapter1
+// Q1
+   alert("hello sir")
+// Q2
+   alert("error! please enter a valid password.")
+// Q3
+   alert("Welcome to JS Land\nHappy Coding!")
+// Q4
+   alert("welcome to JS Land...")
+   alert("Happy Coding!\nPrevent this page from creating additional dialogs.")
+// Q5
+   alert("Hello... i can run JS through my web browser's console")
+// Q6 / Q7
+// checkout to html file
+// chapter 2
+// Q1
+   var username;
+// Q2
+   var myname = "anas usman"
+// Q3
+   var message = "hello world"
+   alert(message)
+// Q4
+   var fullname = "jhone doe"
+   var age = "15 years old"
+   var certificate = "certified mobile application development"
+   alert(fullname)
+   alert(age)
+   alert(certificate)
+// Q5
+   var pizza = "pizza \npizz \npiz \npi \np"
+   alert(pizza)
+// Q6
+   var email = "abc@gmail.com"
+   alert("my email address is "+ email)
+// Q7
+   var book = "a smarter way to learn javascript"
+   alert("i am trying to learn from the book " + book)
+// Q8
+   document.write("yah! i can write html content through javascript <br>")
+// Q9
+   document.write("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬ <br>")
+   alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
+// chapter 3
+// Q1
+   var ages = "i am 15 years old"
+   alert(ages)
+// Q2
+   var visit = "you have visited this site 1 times <br>"
+   document.write(visit )
+// Q3
+var birthyear = "2006"
+document.write("my birth year is " +birthyear+ "<br> data type of my declared variable is number <br>")
+// Q4
+var visitorName = "<b>jhon doe</b>"
+var productTitle  = "<b>T-shirt</b>(s)"
+var quantity = "<b>5</b> "
+document.write(visitorName + " ordered " + quantity + productTitle + " on ABC Clothing Store")
+// chapter 1 to 3
+// CHAPTER 1 ALERT
+// Q1
+alert("anas")
+alert("usman")
+alert("anasusman277@gmail.com")
+alert("079383639363")
+// Q2
+alert("you're learning javascript!")
+// Q3
+alert("hello world")
+// CHAPTER 2 (VARIABLE FOR STRING)
+// Q1
+ var camelCase;
+// Q2
+var b = m;
+var q = "you're genius"
+// Q3
+var teamName = "NINJA"
+var T = "teamName"
+alert(teamName)
+// Q4
+var bestMan = "charlie"
+var o = "bestman"
+alert(bestMan)
+// CHAPTER 3 (VARIABLE FOR NUMBERS)
+// Q1
+var x = "caseQty"
+// Q2
+var caseQty = "144"
+// Q3
+var num = 9;
+var num2 = 4;
+var c = num + num2;
+alert(c)
+// Q4
+var a = "rafiq"
+var b = 5;
+var d = a + b
+document.write(d)
+// Q5
+var merchTotal = 100;
+var shippingCharge = 10;
+
+var orderTotal = merchTotal + shippingCharge;
+alert(orderTotal)
+
+
+// Q6
+var f = 100;
+var e = 400;
+var g = f + e;
+alert(g);
